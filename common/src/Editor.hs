3e5bd5ef0c9bb2515b1bc51702f1a891301d9fc5
Isaac Elliott <isaace71295@gmail.com>
2019-11-05 22:13:53 +0000

    use a path to talk about edits

--- common/src/Editor.hs
+++ common/src/Editor.hs
@@ -1,229 +1,193 @@
-{-# language GADTs, StandaloneDeriving #-}
+{-# language GADTs #-}
 {-# language LambdaCase #-}
-{-# language MultiParamTypeClasses #-}
-{-# language TemplateHaskell #-}
+{-# language OverloadedStrings #-}
+{-# language ScopedTypeVariables #-}
+{-# language StandaloneDeriving #-}
 module Editor where
 
-import Data.Dependent.Map (DMap)
-import Data.Functor.Identity (Identity(..))
--- import Data.Dependent.Sum (ShowTag(..))
-import Data.GADT.Compare (GEq(..), GCompare(..), GOrdering(..))
-import Data.GADT.Compare.TH (deriveGEq, deriveGCompare)
-import Data.GADT.Show.TH (deriveGShow)
-import Data.Map (Map)
-import Data.Set (Set, (\\))
-
-import qualified Data.Dependent.Map as DMap
-import qualified Data.Map as Map
-import qualified Data.Set as Set
-
-data ID a where
-  ID_Expr :: Int -> ID Expr
-  ID_Binding :: Int -> ID Binding
--- fuckn 8.4
-instance Eq (ID a) where
-  (==) a b =
-    case geq a b of
-      Nothing -> False
-      Just{} -> True
-instance Ord (ID a) where
-  compare a b =
-    case gcompare a b of
-      GLT -> LT
-      GEQ{} -> EQ
-      GGT -> GT
-{-
-deriving instance Eq (ID a)
-deriving instance Ord (ID a)
--}
-deriving instance Show (ID a)
-
-data SomeID where
-  SomeID :: ID a -> SomeID
-instance Eq SomeID where
-  (==) (SomeID a) (SomeID b) =
-    case geq a b of
-      Nothing -> False
-      Just{} -> True
-instance Ord SomeID where
-  compare (SomeID a) (SomeID b) =
-    case gcompare a b of
-      GLT -> LT
-      GEQ{} -> EQ
-      GGT -> GT
-deriving instance Show SomeID
-
-data Binding = Binding { unBinding :: String }
-  deriving (Eq, Ord, Show)
+import Control.Monad
+import Control.Monad.Fix
+import Data.String
+import Language.Javascript.JSaddle.Monad (MonadJSM)
+import Reflex
+import Reflex.Network
+import Reflex.Dom
+import qualified GHCJS.DOM.EventM as EventM
+import qualified GHCJS.DOM.GlobalEventHandlers as Events
 
 data Expr
-  = Var String
-  | Hole
+  = Var Int
+  | Lam String Expr
   | App Expr Expr
-  | Lam Binding Expr
   deriving (Eq, Show)
 
-data Context f
-  = Context
-  { _ctxParent :: Maybe SomeID
-  , _ctxLocalScope :: f (Map Binding (ID Binding))
-  }
+data PathPiece a where
+  LamName :: PathPiece String
+  LamBody :: PathPiece Expr
+  AppLeft :: PathPiece Expr
+  AppRight :: PathPiece Expr
+deriving instance Show (PathPiece a)
 
-data NodeInfo f a where
-  BindingInfo ::
-    Context f ->
-    f String ->
-    NodeInfo f Binding
+data Path a b where
+  Nil :: Path a a
+  Cons :: PathPiece a -> Path a b -> Path Expr b
 
-  VarInfo ::
-    Context f ->
-    f String ->
-    f (Set (ID Expr)) ->
-    NodeInfo f Expr
+showPath :: Path a b -> String
+showPath Nil = "<empty path>"
+showPath (Cons a Nil) = show a
+showPath (Cons a b) = show a ++ " > " ++ showPath b
 
-  HoleInfo ::
-    Context f ->
-    NodeInfo f Expr
+data Edit a where
+  EditAt :: Path a b -> b -> Edit a
+  DeleteAt :: Path a b -> Edit a
 
-  AppInfo ::
-    Context f ->
-    ID Expr ->
-    ID Expr ->
-    f (Set (ID Expr)) ->
-    NodeInfo f Expr
+showEdit :: Edit a -> String
+showEdit (EditAt p _) = "Edit at: " ++ showPath p
+showEdit (DeleteAt p) = "Delete at: " ++ showPath p
 
-  LamInfo ::
-    Context f ->
-    ID Binding ->
-    ID Expr ->
-    f (Set (ID Expr)) ->
-    NodeInfo f Expr
-{-
-deriving instance Eq (NodeInfo a)
-deriving instance Ord (NodeInfo a)
--}
--- deriving instance Show1 f => Show (NodeInfo f a)
+data Focus where
+  FocusPath :: Path Expr a -> Focus
 
-context :: NodeInfo f a -> Context f
-context ni =
-  case ni of
-    BindingInfo c _ -> c
-    HoleInfo c -> c
-    VarInfo c _ _ -> c
-    AppInfo c _ _ _ -> c
-    LamInfo c _ _ _ -> c
+data ExprD t
+  = VarD Int
+  | LamD (Dynamic t String) (Dynamic t (ExprD t))
+  | AppD (Dynamic t (ExprD t)) (Dynamic t (ExprD t))
+  | HoleD
 
-freeVars :: Applicative f => NodeInfo f a -> f (Set (ID Expr))
-freeVars ni =
-  case ni of
-    BindingInfo{} -> pure mempty
-    HoleInfo{} -> pure mempty
-    VarInfo _ _ a -> a
-    AppInfo _ _ _ a -> a
-    LamInfo _ _ _ a -> a
+holdExprInner ::
+  (Reflex t, MonadHold t m, Adjustable t m) =>
+  Expr ->
+  Event t (Edit Expr) ->
+  m (ExprD t)
+holdExprInner initial eEdit =
+  case initial of
+    Var n ->
+        pure $ VarD n
+    Lam n b ->
+      LamD <$>
+          (holdDyn
+            n
+            (fmapMaybe
+               (\case
+                   EditAt (Cons LamName Nil) n' -> Just n'
+                   _ -> Nothing)
+               eEdit)) <*>
+        (holdExpr
+            b
+            (fmapMaybe
+               (\case
+                   EditAt (Cons LamBody rest) e -> Just $ EditAt rest e
+                   DeleteAt (Cons LamBody rest) -> Just $ DeleteAt rest
+                   _ -> Nothing)
+               eEdit))
+    App a b ->
+      AppD <$>
+        (holdExpr
+          a
+          (fmapMaybe
+             (\case
+                 EditAt (Cons AppLeft rest) e -> Just $ EditAt rest e
+                 DeleteAt (Cons AppLeft rest) -> Just $ DeleteAt rest
+                 _ -> Nothing)
+             eEdit)) <*>
+        (holdExpr
+          b
+          (fmapMaybe
+             (\case
+                 EditAt (Cons AppRight rest) e -> Just $ EditAt rest e
+                 DeleteAt (Cons AppRight rest) -> Just $ DeleteAt rest
+                 _ -> Nothing)
+             eEdit))
 
-parent :: NodeInfo f a -> Maybe SomeID
-parent = _ctxParent . context
-deriveGEq ''ID
-deriveGCompare ''ID
-deriveGShow ''ID
+holdExprM ::
+  (Reflex t, MonadHold t m, Adjustable t m) =>
+  m (ExprD t) ->
+  Event t (Edit Expr) ->
+  m (Dynamic t (ExprD t))
+holdExprM initialM eEdit =
+  networkHold
+    initialM
+    (fmapMaybe
+        (\case
+            EditAt Nil a -> Just $ holdExprInner a eEdit
+            DeleteAt Nil -> Just $ pure HoleD
+            _ -> Nothing)
+        eEdit)
 
-{-
-instance ShowTag ID NodeInfo where
-  showTaggedPrec ID_Expr{} = showsPrec
-  showTaggedPrec ID_Bound{} = showsPrec
-  showTaggedPrec ID_Binding{} = showsPrec
--}
+holdExpr ::
+  (Reflex t, MonadHold t m, Adjustable t m) =>
+  Expr ->
+  Event t (Edit Expr) ->
+  m (Dynamic t (ExprD t))
+holdExpr initial eEdit = holdExprM (holdExprInner initial eEdit) eEdit
 
-class Unbuild a where
-  unbuild ::
-    Applicative f =>
-    DMap ID (NodeInfo f) ->
-    Context f ->
-    a ->
-    (ID a, DMap ID (NodeInfo f), f (Set (ID Expr)))
+deleteEvent ::
+  ( Reflex t
+  , DomBuilderSpace m ~ GhcjsDomSpace
+  , TriggerEvent t m
+  , HasDocument m
+  , MonadJSM m
+  ) =>
+  Dynamic t Focus ->
+  m (Event t (Edit Expr))
+deleteEvent dFocus = do
+  doc <- askDocument
+  eDeleteKey <-
+    wrapDomEventMaybe doc (`EventM.on` Events.keyDown) $ (\c -> guard $ c == 46) <$> getKeyEvent
+  pure $ (\(FocusPath p) -> DeleteAt p) <$> current dFocus <@ eDeleteKey
 
-instance Unbuild Binding where
-  unbuild m ctx (Binding a) =
-    let
-      i' = (ID_Binding $ DMap.size m)
-    in
-      (i', DMap.insert i' (BindingInfo ctx $ pure a) m, pure mempty)
 
-instance Unbuild Expr where
-  unbuild m ctx (Var a) =
-    let
-      i' = ID_Expr $ DMap.size m
-      fvs = pure $ Set.singleton i'
-    in
-      ( i'
-      , DMap.insert i' (VarInfo ctx (pure a) fvs) m
-      , fvs
-      )
-  unbuild m ctx Hole =
-    let
-      i' = (ID_Expr $ DMap.size m)
-    in
-      (i', DMap.insert i' (HoleInfo ctx) m, pure mempty)
-  unbuild m ctx (App a b) =
-    let
-      (a', m', fvs') = unbuild m (ctx { _ctxParent = Just $ SomeID i'}) a
-      (b', m'', fvs'') = unbuild m' (ctx { _ctxParent = Just $ SomeID i'}) b
-      i' = (ID_Expr $ DMap.size m'')
-      fvs''' = (<>) <$> fvs' <*> fvs''
-    in
-      (i', DMap.insert i' (AppInfo ctx a' b' fvs''') m'', fvs''')
-  unbuild m ctx (Lam a b) =
-    let
-      (a', m', _) = unbuild m (ctx { _ctxParent = Just $ SomeID i'}) a
-      (b', m'', fvs') = unbuild m' (Context (Just $ SomeID i') (Map.insert a a' <$> _ctxLocalScope ctx)) b
-      i' = (ID_Expr $ DMap.size m'')
-      fvs'' = (\bbs ffs -> ffs \\ Set.fromList bbs) <$> getBounds m'' a' <*> fvs'
-    in
-      (i', DMap.insert i' (LamInfo ctx a' b' fvs'') m'', fvs'')
-
-rebuild :: DMap ID (NodeInfo Identity) -> ID a -> Maybe a
-rebuild m i =
-  DMap.lookup i m >>=
-  \case
-    BindingInfo _ a -> Just $ Binding $ runIdentity a
-    HoleInfo _ -> Just Hole
-    VarInfo _ val _ -> Just $ Var $ runIdentity val
-    AppInfo _ a b _ -> App <$> rebuild m a <*> rebuild m b
-    LamInfo _ a b _ -> Lam <$> rebuild m a <*> rebuild m b
-
-getBounds :: Applicative f => DMap ID (NodeInfo f) -> ID Binding -> f [ID Expr]
-getBounds m i =
-  DMap.foldrWithKey
-    (\k v rest ->
-       case v of
-         VarInfo ctx val _ ->
-           (\vv cc rr -> case Map.lookup (Binding vv) cc of
-             -- this variable is not bound by local scope
-             Nothing -> rr
-             -- this variable is bound by local scope
-             Just b ->
-               if b == i
-               -- this variable is bound by the one we're interested in
-               then k : rr
-               -- this variable is bound by another variable
-               else rr) <$>
-           val <*>
-           _ctxLocalScope ctx <*>
-           rest
-         _ -> rest)
-    (pure [])
-    m
-
-getBinding ::
-  DMap ID (NodeInfo Identity) ->
-  ID Expr ->
-  Maybe (ID Binding)
-getBinding m i =
-  DMap.lookup i m >>=
-  \case
-    VarInfo ctx val _ ->
-      Map.lookup
-        (Binding $ runIdentity val)
-        (runIdentity $ _ctxLocalScope ctx)
-    _ -> Nothing
+viewExprD ::
+  forall t m.
+  (DomBuilder t m, PostBuild t m, MonadHold t m, MonadFix m) =>
+  Dynamic t Focus ->
+  Dynamic t (ExprD t) ->
+  m (Event t (Edit Expr))
+viewExprD df dExpr = do
+  switchHold never =<< dyn (go id (Just <$> df) <$> dExpr)
+  where
+    go ::
+      (Path Expr Expr -> Path Expr Expr) ->
+      Dynamic t (Maybe Focus) ->
+      ExprD t ->
+      m (Event t (Edit Expr))
+    go path dFocus expr = do
+      dIsFocused <- holdUniqDyn $ (\case; Just (FocusPath Nil) -> True; _ -> False) <$> dFocus
+      let dAttrs = (\b -> if b then "style" =: "background-color: gray;" else mempty) <$> dIsFocused
+      (_, e1) <-
+        elDynAttr' "span" dAttrs $
+          case expr of
+            VarD n -> do
+              text . fromString . ('#' :) $ show n
+              pure never
+            LamD _ b -> do
+              text "lam. "
+              let
+                dFocus' =
+                  (\case
+                      Just (FocusPath (Cons LamBody rest)) -> Just (FocusPath rest)
+                      _ -> Nothing) <$>
+                  dFocus
+              switchHold never =<< dyn (go (path . Cons LamBody) dFocus' <$> b)
+            AppD a b -> do
+              let
+                dFocus' =
+                  (\case
+                      Just (FocusPath (Cons AppLeft rest)) -> Just (FocusPath rest)
+                      _ -> Nothing) <$>
+                  dFocus
+              e1 <- switchHold never =<< dyn (go (path . Cons AppLeft) dFocus' <$> a)
+              text " "
+              let
+                dFocus'' =
+                  (\case
+                      Just (FocusPath (Cons AppRight rest)) -> Just (FocusPath rest)
+                      _ -> Nothing) <$>
+                  dFocus
+              e2 <- switchHold never =<< dyn (go (path . Cons AppRight) dFocus'' <$> b)
+              pure $ leftmost [e1, e2]
+            HoleD -> do
+              text "_"
+              pure never
+      let e2 = never
+      pure $ leftmost [e1, e2]
